5dd01d4f4b4e3e8a07478ce2c34d98c4acbe9ff2
awmleer <awmleer@sparker.xyz>
2017-08-15 23:33:08 +0800

    [feat] collection page

--- src/pages/collection/collection.ts
+++ src/pages/collection/collection.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {CollectionService} from "../../services/collection.service";
+import {BookCollection} from "../../classes/collection";
+import {BookDetailPage} from "../book-detail/book-detail";
 
 @Component({
   selector: 'page-collection',
@@ -7,8 +10,19 @@ import { NavController } from 'ionic-angular';
 })
 export class CollectionPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private navCtrl: NavController,
+    private collectionSvc: CollectionService
+  ) {}
 
+  get bookCollections():BookCollection[]{
+    return this.collectionSvc.collections;
+  }
+
+  goBookDetail(bookCollection:BookCollection){
+    this.navCtrl.push(BookDetailPage,{
+      'id':bookCollection.bookId
+    });
   }
 
 }
