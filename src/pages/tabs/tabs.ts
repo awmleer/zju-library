a94f04dab65cc53f62fd3f7e99a88b4beb617dd7
awmleer <awmleer@sparker.xyz>
2017-08-15 21:08:39 +0800

    [feat] about page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -10,8 +10,8 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = ContactPage;
+  tab3Root = AboutPage;
 
   constructor() {
 
