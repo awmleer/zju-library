810021ac8aa220a90a10f0ae2ec8e1ec28655f51
awmleer <awmleer@sparker.xyz>
2017-08-15 18:18:21 +0800

    [add] loading

--- src/pages/book-detail/book-detail.ts
+++ src/pages/book-detail/book-detail.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, Loading, LoadingController, NavController, NavParams} from 'ionic-angular';
 import {LibraryService} from "../../services/library.service";
 import {BookDetail, BookItem} from "../../classes/book";
 
@@ -15,12 +15,22 @@ export class BookDetailPage {
   base:string;
   book:BookDetail;
   items:BookItem[];
+  loading:Loading;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private librarySvc: LibraryService
+    private librarySvc: LibraryService,
+    private loadingCtrl: LoadingController
   ) {}
+
+  ionViewWillLoad(){
+    this.loading=this.loadingCtrl.create({
+      spinner: 'dots',
+      content: '加载中...'
+    });
+    this.loading.present();
+  }
 
   ionViewDidLoad(){
     this.id=this.navParams.get('id');
@@ -28,11 +38,12 @@ export class BookDetailPage {
     this.librarySvc.bookDetail(this.base,this.id).then(book=>{
       console.log(book);
       this.book=book;
+      this.librarySvc.bookItems(this.base,this.id).then(items=>{
+        console.log(items);
+        this.items=items;
+        this.loading.dismiss();
+      });
     });
-    this.librarySvc.bookItems(this.base,this.id).then(items=>{
-      console.log(items);
-      this.items=items;
-    })
   }
 
 }
