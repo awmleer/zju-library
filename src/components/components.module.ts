d02ca403cde96e335d89f1d5a3138d9f33ab02f0
awmleer <awmleer@sparker.xyz>
2017-08-15 19:12:09 +0800

    [add] search box

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SearchBoxComponent } from './search-box/search-box';
+import {IonicModule} from "ionic-angular";
+
 @NgModule({
 	declarations: [SearchBoxComponent],
-	imports: [],
+	imports: [
+    IonicModule,
+  ],
 	exports: [SearchBoxComponent]
 })
 export class ComponentsModule {}
