ec1050fcd0b3324f82874ce72662077604d8fbd0
awmleer <awmleer@sparker.xyz>
2017-08-24 17:50:27 +0800

    [!] bug fixes

--- src/app/const.ts
+++ src/app/const.ts
@@ -1,7 +1,7 @@
 export const CONST={
   libraryUrl:window['cordova']?'http://webpac.zju.edu.cn':'/zju',
   ossUrl:'http://qiniu.zjulibrary.sparker.top',
-  doubanUrl:window['cordova']?'http://qiniu.zjulibrary.sparker.top':'/douban',
+  doubanUrl:window['cordova']?'https://api.douban.com':'/douban',
   version:{
     major:1,
     minor:1,
